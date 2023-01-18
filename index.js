e7d3872d26affd37fe5eae82395b1c346dabe2b3
juliaandreds <julia.andre@hotmail.com>
2023-01-18 20:01:49 -0300

    async/await

--- index.js
+++ index.js
@@ -5,25 +5,32 @@ function trataErro (erro) {
     throw new Error (chalk.red (erro.code, 'Não há arquivo no diretório'));
 }
 
-function pegaArquivo (caminhoArquivo) {
+//async
+async function pegaArquivo (caminhoArquivo) {
+    try {
+        const encoding = "UTF-8";
+        const texto = await fs.promises.readFile (caminhoArquivo, encoding) 
+        console.log (chalk.green(texto))
+    } catch (erro) {
+        trataErro (erro)
+    }
+}
+
+
+
+
+//async/await
+/* function pegaArquivo (caminhoArquivo) {
     const encoding = "UTF-8";
     fs.promises
     .readFile (caminhoArquivo,encoding)
     .then ((texto) => console.log (chalk.green(texto)))
     .catch (trataErro)
-}
+*/
 
-/* function pegaArquivo (caminhoArquivo) {
-    const encoding = "UTF-8";
-    fs.readFile(caminhoArquivo, encoding, (erro, texto)  => {
-        if (erro ) {
-            trataErro (erro);
-        }
-        console.log (chalk.green(texto));
-    }) */
 
 
 
 pegaArquivo ("./arquivos/texto.md")
-
+pegaArquivo("./arquivos")
 
