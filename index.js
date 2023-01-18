26b80b6f5352ffa3ac2f476db1368e9d7cab16bc
juliaandreds <julia.andre@hotmail.com>
2023-01-18 19:07:43 -0300

    Tratamento de erros

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import fs from "fs";
 import chalk from "chalk";
 
+function trataErro (erro) {
+    throw new Error (chalk.red (erro.code, 'Não há arquivo no diretório'));
+}
+
 function pegaArquivo (caminhoArquivo) {
     const encoding = "UTF-8";
-    fs.readFile(caminhoArquivo, encoding, (_, texto)  => {
+    fs.readFile(caminhoArquivo, encoding, (erro, texto)  => {
+        if (erro ) {
+            trataErro (erro);
+        }
         console.log (chalk.green(texto));
     })
 
 }
 
-pegaArquivo ("./arquivos/texto.md")
+pegaArquivo ("./arquivos")
 
 
