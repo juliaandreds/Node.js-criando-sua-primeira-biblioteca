1466ef602be4c4f82ecdae9b520da659751af7c6
Heberth Alexandre <121596722+juliaandreds@users.noreply.github.com>
2023-01-19 23:10:45 -0300

    Adicionando opções

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,30 @@
 import chalk from "chalk";
 import pegaArquivo from "./index.js";
 import fs from "fs";
+import listaValidada from "./http-validacao.js";
+
 
 const caminho = process.argv;
 
+function imprimeLista(valida, resultado, identificador = "") {
 
-
-function imprimeLista(resultado, identificador = "") {
-  console.log(
-    chalk.yellow("Lista de links"), 
-    chalk.black.bgGreen(identificador), resultado
-  );
+  if(valida) {
+    console.log()
+    console.log(
+      chalk.yellow("Lista validada"), 
+      chalk.black.bgGreen(identificador), listaValidada(resultado)
+    );
+  } else {
+    console.log(
+      chalk.yellow("Lista de links"), 
+      chalk.black.bgGreen(identificador), resultado
+    );
+  }
 }
 
 async function processaTexto(argumentos) {
   const caminho = argumentos[2];
+  const valida = argumentos [3] === '--valida';
 
   try {
     fs.lstatSync (caminho)
@@ -28,7 +38,7 @@ async function processaTexto(argumentos) {
   if(fs.lstatSync(caminho).isFile()){
     const resultado = await pegaArquivo(argumentos[2]);
     
-    imprimeLista(resultado);
+    imprimeLista(valida, resultado);
 
   } else if(fs.lstatSync(caminho).isDirectory()){ 
     const arquivos = await fs.promises.readdir(caminho);
@@ -36,7 +46,7 @@ async function processaTexto(argumentos) {
     arquivos.forEach(async (nomeDeArquivo) => {
       const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
 
-      imprimeLista(lista, nomeDeArquivo);
+      imprimeLista(valida, lista, nomeDeArquivo);
     });
     
   }
---
+++ src/http-validacao.js
@@ -0,0 +1,3 @@
+export default function listaValidada (listadeLinks) {
+    return 'entrou na função';
+}
