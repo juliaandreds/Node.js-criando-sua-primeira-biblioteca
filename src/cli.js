5d083f3c501c86a6aa014d226828adc3d5974a03
Heberth Alexandre <121596722+juliaandreds@users.noreply.github.com>
2023-01-19 21:26:37 -0300

    Processando diretórios

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,33 @@
 import chalk from "chalk";
 import pegaArquivo from "./index.js";
+import fs from "fs";
 
 const caminho = process.argv;
-console.log(caminho)
 
-async function processaTexto(caminho) {
-  const resultado = await pegaArquivo(caminho[2])
-  console.log (chalk.yellow('lista de links'), resultado)
+function imprimeLista(resultado) {
+  console.log(chalk.yellow("Lista de links"), resultado);
+}
+
+async function processaTexto(argumentos) {
+  const caminho = argumentos[2];
+
+  if(fs.lstatSync(caminho).isFile()){
+    const resultado = await pegaArquivo(argumentos[2]);
+    
+    imprimeLista(resultado);
+
+  } else if(fs.lstatSync(caminho).isDirectory()){ 
+    const arquivos = await fs.promises.readdir(caminho);
+
+    arquivos.forEach(async (nomeDeArquivo) => {
+      const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+
+      imprimeLista(lista);
+    });
+    
+    console.log("Arquivos: ", arquivos);
+  }
+
 }
 
 processaTexto(caminho);
