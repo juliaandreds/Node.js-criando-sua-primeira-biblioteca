bc16f17319527d859547ff7cc5a9e65059fde346
Heberth Alexandre <121596722+juliaandreds@users.noreply.github.com>
2023-01-20 13:53:48 -0300

    Montando o objeto

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
  function extraiLinks (arrLinks) {
    return arrLinks.map((objetoLink) => Object.values (objetoLink).join())
 }
@@ -6,17 +8,34 @@ async function checaStatus (listaURLs) {
     const arrStatus = await Promise
     .all (
         listaURLs.map (async(url) => {
-        const response = await fetch(url)
-        return response.status
-        })  
+        try {
+            const response = await fetch(url);
+            return response.status;
+
+        }   catch (erro) {
+            manejaErros (erro);
+        } })  
     )
     return arrStatus; 
 }
 
+function manejaErros (erro) {
+    if (erro.cause.code === 'ENOTFOUND') {
+        return 'Link não encontrado'
+    }  else {
+        return 'Ocorreu algum erro'
+    }
+}
+
 export default async function listaValidada (listadeLinks) {
     const links =  extraiLinks (listadeLinks);
-    const status = await checaStatus (links)
-    return status
+    const status = await checaStatus (links);
+
+    return listadeLinks.map ((objeto,indice) => ({
+        ...objeto,
+        status: status[indice]
+    })
+    )
 }
 
 //[gatinho salsicha](http://gatinhosalsicha.com.br/)
